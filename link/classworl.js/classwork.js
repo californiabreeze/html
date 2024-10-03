4bde8cc48fb4b58d2b8feae7cff74ff3f4f6af43
californiabreeze <kingsleykingsolo1@gmail.com>
2024-10-03 16:08:06 +0100

    money

---
+++ link/classworl.js/classwork.js
@@ -0,0 +1,13 @@
+const person={
+    firstname:"osas",
+    lastname:"monday",
+    age:"70",
+    eyecolor:"red",
+    isMarried: true,
+    likes:["reading","writing"],
+    schools:{
+        primary:"Edokpolor",
+        secondary:"secondary school"
+    }
+}
+console.log(person.likes[0],person.schools.secondary)
